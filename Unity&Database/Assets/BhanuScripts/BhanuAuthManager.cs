c7800d5d8803f53d7380d75108364219b3e1598f
uabhanu <uabhanu@gmail.com>
2017-12-14 12:09:07 -0800

    Lecture 11 Up Next

--- Unity&Database/Assets/BhanuScripts/BhanuAuthManager.cs
+++ Unity&Database/Assets/BhanuScripts/BhanuAuthManager.cs
@@ -5,13 +5,16 @@ using UnityEngine.UI;
 
 public class BhanuAuthManager : MonoBehaviour 
 {
-	[SerializeField] GameObject m_joinGameButtonObj , m_mainMenuObj , m_registerButtonObj , m_swapRegistrationButtonObj;
+	bool m_isShowingRegistration = false;
+
+	[SerializeField] GameObject m_joinGameButtonObj , m_mainMenuObj , m_signInButtonObj , m_submitButtonObj , m_swapRegistrationButtonObj;
 	[SerializeField] GameObject m_emailFieldObj , m_passwordFieldObj , m_passwordAgainFieldObj;
-	[SerializeField] Text m_emailText , m_passwordText , m_passwordAgainText;
+	[SerializeField] Text m_email , m_messageToUser , m_password , m_passwordAgain , m_swapButton;
 
 	void Start() 
 	{
-		LogInPanel();
+		m_messageToUser.text = "";
+		JoinPanel();
 	}
 
 	void Update() 
@@ -19,25 +22,52 @@ public class BhanuAuthManager : MonoBehaviour
 		
 	}
 
-	public void LogInButtonTapped()
+	public void JoinGameTapped()
 	{
-		
+		m_messageToUser.text = "Joining...";
 	}
 
-	public void LogInPanel()
+	public void JoinPanel()
 	{
 		m_joinGameButtonObj.SetActive(true);
-		m_registerButtonObj.SetActive(false);
 		m_passwordAgainFieldObj.SetActive(false);
+		m_submitButtonObj.SetActive(false);
 	}
 
 	public void RegisterButtonTapped()
 	{
-		
+		m_messageToUser.text = "Registering...";
+	}
+
+	public void SignInButtonTapped()
+	{
+		m_messageToUser.text = "Signing In...";
+	}
+
+	public void SignInPanel()
+	{
+		m_passwordAgainFieldObj.SetActive(false);
+		m_signInButtonObj.SetActive(true);
+		m_submitButtonObj.SetActive(false);
 	}
 
 	public void SwapSignUpSignIn()
 	{
-		
+		if(m_isShowingRegistration) 
+		{
+			m_isShowingRegistration = false;
+			m_joinGameButtonObj.SetActive(true);
+			m_passwordAgainFieldObj.SetActive(false);
+			m_submitButtonObj.SetActive(false);
+			m_swapButton.text = "Sign Up";
+		} 
+		else 
+		{
+			m_isShowingRegistration = true;	
+			m_joinGameButtonObj.SetActive(false);
+			m_passwordAgainFieldObj.SetActive(true);
+			m_submitButtonObj.SetActive(true);
+			m_swapButton.text = "Sign In";
+		}
 	}
 }
