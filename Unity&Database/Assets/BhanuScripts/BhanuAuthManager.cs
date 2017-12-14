a6401bda65d0eb41e2ce109328287010f9886814
uabhanu <uabhanu@gmail.com>
2017-12-14 10:51:31 -0800

    Lecture 8 Up Next

---
+++ Unity&Database/Assets/BhanuScripts/BhanuAuthManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BhanuAuthManager : MonoBehaviour 
+{
+	[SerializeField] GameObject m_joinGameButtonObj , m_mainMenuObj , m_registerButtonObj , m_swapRegistrationButtonObj;
+	[SerializeField] GameObject m_emailFieldObj , m_passwordFieldObj , m_passwordAgainFieldObj;
+	[SerializeField] Text m_emailText , m_passwordText , m_passwordAgainText;
+
+	void Start() 
+	{
+		LogInPanel();
+	}
+
+	void Update() 
+	{
+		
+	}
+
+	public void LogInButtonTapped()
+	{
+		
+	}
+
+	public void LogInPanel()
+	{
+		m_joinGameButtonObj.SetActive(true);
+		m_registerButtonObj.SetActive(false);
+		m_passwordAgainFieldObj.SetActive(false);
+	}
+
+	public void RegisterButtonTapped()
+	{
+		
+	}
+
+	public void SwapSignUpSignIn()
+	{
+		
+	}
+}
